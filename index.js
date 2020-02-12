998a35f00065c325a0d85f832475f12bb02009d2
erjanmx <erjanmx@gmail.com>
2020-02-12 14:25:37 +0100

    f

--- index.js
+++ index.js
@@ -31,13 +31,9 @@ const logger = winston.createLogger({
     }),
   ),
   transports: [ 
-    new winston.transports.File({ filename: 'log.log' })
+    new winston.transports.Console()
   ]
 });
-
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console());
-}
 
 const timeZone = process.env.TIME_ZONE || "Asia/Almaty"; 
 
