f2317d6615db45a139f400bb0f9fc91aa0ef7905
erjanmx <erjanmx@gmail.com>
2022-06-02 10:28:58 +0200

    Update script

--- update-topics.sh
+++ update-topics.sh
@@ -1,10 +1,12 @@
-!/bin/bash
+#!/bin/bash
 
 git reset --hard origin/master
 git pull
 
+source ~/.nvm/nvm.sh
 npm start
 
 git add db/topics.json
 git commit -m "Update topics"
 git push origin master
+
