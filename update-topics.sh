4b7d7991fc5f18858f0a4f98e2e14ff07476ee44
erjanmx <erjanmx@gmail.com>
2024-01-29 19:48:24 +0100

    Update script

--- update-topics.sh
+++ update-topics.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-#git reset --hard origin/master
-#git pull
+git reset --hard origin/master
+git pull
 
 source ~/.nvm/nvm.sh
-nvm install 12 && npm install && npm start
+nvm install 12
+npm install
+npm start
 
 git add db/topics.json
 git commit -m "Update topics"
 git push origin master
+
