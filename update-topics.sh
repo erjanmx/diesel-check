7405c3c3b082a4e1be79c803cfcdae69755a0ba4
server <server@pop-os.localdomain>
2024-01-29 19:45:23 +0100

    Update script

--- update-topics.sh
+++ update-topics.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-git reset --hard origin/master
-git pull
+#git reset --hard origin/master
+#git pull
 
 source ~/.nvm/nvm.sh
-npm start
+nvm install 12 && npm install && npm start
 
 git add db/topics.json
 git commit -m "Update topics"
